42d686d3a60afd07d6062b3b1d51072a156acc30
Nicole LaSusa <nicolelasusa@gmail.com>
2020-07-08 22:34:41 -0400

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// ON CLICK EVENTS 
+$("#back").on('click', function(){
+    window.location = "https://ktmc.com/";    
+  });
+
+$(".fancy_link").on('click', function (){
+  window.location ="https://www.ktmc.com/webfiles/Letter%20from%20the%20Hon_%20Kathaleen%20S_%20McCormick%20informing%20counsel%20that%20the%20J....pdf"
+}); 
